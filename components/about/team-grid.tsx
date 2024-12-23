f02a3b2c2d6bcc48e61f4b3ffd5949e6cb4e993c
Linkai Wu <linkai@linkaiwu.com>
2024-12-23 00:52:30 -0500

    add team grid responsiveness

--- components/about/team-grid.tsx
+++ components/about/team-grid.tsx
@@ -13,7 +13,7 @@ interface TeamGridProps {
 
 export function TeamGrid({ children, className, ...rest }: TeamGridProps) {
   return (
-    <div className={cn("grid grid-cols-3 gap-x-12 gap-y-8", className)} {...rest}>
+    <div className={cn("grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-8", className)} {...rest}>
       {children}
     </div>
   );
