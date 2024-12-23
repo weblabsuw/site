545b9c37dd704316742d1e788c38da5fe0dcdcea
Linkai Wu <linkai@linkaiwu.com>
2024-12-22 23:15:27 -0500

    add rehype plugins

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,6 @@
 import type { MDXComponents } from 'mdx/types';
- 
+import "@/components/github-dark-dimmed.css"; // theme for rehype-highlight
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import rehypeHighlight from "rehype-highlight";
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
@@ -9,7 +11,7 @@ const nextConfig: NextConfig = {
 const withMDX = createMDX({
   options: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
+    rehypePlugins: [rehypeSlug, rehypeHighlight],
   },
 });
 
