80dc345adeeed977f22fdcd8524beeb737883f00
Linkai Wu <linkai@linkaiwu.com>
2025-01-31 10:56:44 -0600

    remove events page route

--- app/events/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllEvents } from "@/lib/events";
-
-import { EventsPage } from "@/components/events";
-
-export default async function Page() {
-  const events = await getAllEvents();
-
-  return (<EventsPage events={events}/>);
-}
