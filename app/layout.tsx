67126e15d6979f4c5dacf901c10cc3b257e841d2
Tsuni <78718829+TetraTsunami@users.noreply.github.com>
2025-09-18 00:49:40 -0500

    Add webs in footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${unbounded.variable} antialiased bg-surface text-onSurface`}
+        className={`${geistSans.variable} ${geistMono.variable} ${unbounded.variable} antialiased bg-surface text-onSurface relative`}
       >
         {children}
         <Toaster
--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,12 +1,18 @@
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import { Hero } from "./hero";
+import { Web } from "@/components/web";
 // import { Slideshow } from "./slideshow";
 import { Events } from "./events";
 
 export function HomePage() {
   return (
     <>
+      <div className="absolute inset-0 -z-10 overflow-hidden">
+        <Web className="text-primary/5 absolute scale-150 -bottom-[300px] -left-[250px] rotate-[210deg]" />
+        <Web className="text-primary/5 absolute scale-150 -bottom-[250px] -right-[250px] rotate-[30deg]" />
+      </div>
+
       <Navbar active="home" />
 
       <Hero />
